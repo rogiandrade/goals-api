fc5bd147bf517bf4e1b9c729fc24293306bee990
Igor Andrade <rogiandradee@gmail.com>
2024-03-15 18:32:37 +0000

    REFACTOR migration

---
+++ prisma/migrations/20240308095747_api_models/migration.sql
@@ -0,0 +1,38 @@
+-- CreateTable
+CREATE TABLE "goals" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "goal_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "goal_id" TEXT NOT NULL,
+    "week_day" INTEGER NOT NULL,
+    CONSTRAINT "goal_week_days_goal_id_fkey" FOREIGN KEY ("goal_id") REFERENCES "goals" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "day_goals" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "goal_id" TEXT NOT NULL,
+    CONSTRAINT "day_goals_day_id_fkey" FOREIGN KEY ("day_id") REFERENCES "days" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "day_goals_goal_id_fkey" FOREIGN KEY ("goal_id") REFERENCES "goals" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "goal_week_days_goal_id_week_day_key" ON "goal_week_days"("goal_id", "week_day");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "days_date_key" ON "days"("date");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "day_goals_day_id_goal_id_key" ON "day_goals"("day_id", "goal_id");
